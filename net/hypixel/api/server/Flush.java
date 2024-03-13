b741a0dbb559d0d7de033aeece50efac009b1259
Pryrobite <143216072+Pryrobite@users.noreply.github.com>
2024-03-13 04:41:02 -0500

    Create Flush.java

---
+++ net/hypixel/api/server/Flush.java
@@ -0,0 +1,53 @@
+package net.hypixel.api.player;
+
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Item;
+import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+import java.net.Socket;
+
+
+public class CostomeNameBlock extends JavaPlugin {
+
+    /**
+     * @Hypixel {Link} onPlayer - Event @{Link}
+     */
+
+    public boolean onPlayer(CommandSender sender, Command cmd, Player player, Socket socket) {
+      /** @Hypixel {Link} onPlayer */
+        if (player.isOnline() == false) {
+            player.chat("Hypixel: Player " + player.getName() + " Has left the lobby!");
+        }
+        if (player.isOnline() == true) {
+
+
+            if (player.performCommand("/Flush")) {
+
+                socket.isClosed();
+                socket.getInetAddress().isMCGlobal();
+            }
+        }
+        this.onEnable();
+        return true;
+    }
+    public void onEnable() {
+        this.onEnable();
+    }
+
+    public void onDisable() {
+        /** @Hypixel {link} */
+        this.onDisable();
+        if (true) {
+            System.out.println(" Server is Ending");
+        }
+    }
+    public void save() {
+        Player player = null;
+        System.getenv(player.getName());
+        System.out.println("Server is saving!" + System.out.checkError());
+    }
+}
